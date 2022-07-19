8c04aadab6eb822b11d3520fa41cfdeb85ba1d7d
NtsakoMphongola <105928245+NtsakoMphongola@users.noreply.github.com>
2022-07-19 12:49:51 +0200

    init

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import React, { Component } from 'react';
+import { AppRegistry, View, Image, StyleSheet } from 'react-native';
+import splash from './assets/757889.jpeg';
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+  },
+  tinyLogo: {
+    width: 280,
+    height: 420,
+  },
+  logo: {
+    width: 66,
+    height: 58,
   },
 });
+
+class DisplayAnImage extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image
+          style={styles.tinyLogo}
+          source={{uri: splash}}
+        />
+        <Image
+          style={styles.tinyLogo}
+          source={{uri: 'https://kinsta.com/wp-content/uploads/2020/09/imag-file-types-1024x512.png'}}
+        />
+        <Image
+          style={styles.logo}
+          source={{uri: 'https://images.pexels.com/photos/1133957/pexels-photo-1133957.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'}}
+        />
+      </View>
+    );
+  }
+}
+
+export default DisplayAnImage;
